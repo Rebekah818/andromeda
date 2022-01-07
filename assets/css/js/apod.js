22f526bd473f03f2bc4345a15c8a9a73575a4148
marissann11 <wilhite.marissa@gmail.com>
2022-01-07 13:04:20 -0500

    added apod.js file

---
+++ assets/css/js/apod.js
@@ -0,0 +1,21 @@
+let apodImage = document.querySelector("#picture")
+
+async function getApod(){
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=CamLBC9eJwRQEcdg2oafBlPIrOFX5MqqPM8oazvO`);
+    const res = await response.json();
+
+    let copyright = res.copyright;
+    console.log(copyright);
+
+    let date = res.date;
+    console.log(date);
+
+    let explanation = res.explanation;
+    console.log(explanation);
+
+    let imageUrl = res.hdurl;
+    const imageContainer = document.createElement("img");
+    imageContainer.setAttribute("src", imageUrl);
+    imageContainer.append(apodImage)
+}
+getApod();
