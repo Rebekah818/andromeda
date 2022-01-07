863ba47b94eeeb0195979f51d981ef1df03faeee
marissann11 <wilhite.marissa@gmail.com>
2022-01-07 13:08:04 -0500

    added apod js file

--- assets/css/js/apod.js
+++ assets/css/js/apod.js
@@ -1,4 +1,4 @@
-let apodImage = document.querySelector("#picture")
+//let apodImage = document.querySelector("#picture")
 
 async function getApod(){
     const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=CamLBC9eJwRQEcdg2oafBlPIrOFX5MqqPM8oazvO`);
@@ -13,9 +13,9 @@ async function getApod(){
     let explanation = res.explanation;
     console.log(explanation);
 
-    let imageUrl = res.hdurl;
-    const imageContainer = document.createElement("img");
-    imageContainer.setAttribute("src", imageUrl);
-    imageContainer.append(apodImage)
+    // let imageUrl = res.hdurl;
+    // const imageContainer = document.createElement("img");
+    // imageContainer.setAttribute("src", imageUrl);
+    // imageContainer.append(apodImage)
 }
 getApod();
