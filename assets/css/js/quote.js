e62428b9e45fbdb56bfea2980330b35ba8c08582
Sohrob1 <94089858+Sohrob1@users.noreply.github.com>
2022-01-07 13:03:04 -0500

    Add files via upload

---
+++ assets/css/js/quote.js
@@ -0,0 +1,10 @@
+var getQuoteArray = function(apiURL) {
+    fetch("https://type.fit/api/quotes")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+      var randomQuote = data[Math.floor(Math.random() * (1643 - 0 + 1) + 0)].text;
+     return randomQuote
+  });
+};
