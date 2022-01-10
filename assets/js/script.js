07084ac206c9b00281f8e0609a0042c23cd52637
Rebekah 2021 <rebekahmcdevitt@gmail.com>
2022-01-10 13:06:36 -0500

    udate html and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
 
-  $(document).ready(function(){
-    $('.fixed-action-btn').floatingActionButton();
+$(document).ready(function () {
+  $('.fixed-action-btn').floatingActionButton();
     .then()
+});
+
+
+$("button").on(click(function () {
+  $("button").click(function () {
+    $(this).click()
   });
+}));
