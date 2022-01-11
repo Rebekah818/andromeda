97d319f400a221746c688bbde851bedfa5931a32
marissann11 <wilhite.marissa@gmail.com>
2022-01-10 19:43:41 -0500

    refactored getApod function

--- assets/css/js/apod.js
+++ assets/css/js/apod.js
@@ -1,9 +1,12 @@
-let apodImage = document.querySelector("#space-img")
+let apodImage = document.querySelector("#space-img");
 
-async function getApod(){
-    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=CamLBC9eJwRQEcdg2oafBlPIrOFX5MqqPM8oazvO`);
-    const res = await response.json();
+async function getApod() {
+  const response = await fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=CamLBC9eJwRQEcdg2oafBlPIrOFX5MqqPM8oazvO`
+  );
+  const res = await response.json();
 
+  if (res.media_type == "image") {
     let copyright = res.copyright;
     console.log(copyright);
 
@@ -15,6 +18,12 @@ async function getApod(){
 
     let imageUrl = res.hdurl;
 
-    document.querySelector("#space-img").style.backgroundImage = "url(" + imageUrl + ")"
+    document.querySelector("#space-img").style.backgroundImage =
+      "url(" + imageUrl + ")";
+  } else {
+    let backupUrl = "https://apod.nasa.gov/apod/image/2110/SH2-308NS.jpg";
+    document.querySelector("#space-img").style.backgroundImage =
+      "url(" + backupUrl + ")";
+  }
 }
 getApod();
